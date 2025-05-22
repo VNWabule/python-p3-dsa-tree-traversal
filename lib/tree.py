4669c74e308728cc33fbdc719bc403a1a317317a
VNWabule <vicwabule@gmail.com>
2025-05-22 15:49:23 +0300

    done

--- lib/tree.py
+++ lib/tree.py
@@ -2,5 +2,14 @@ class Tree:
   def __init__(self, root = None):
     self.root = root
 
-  def get_element_by_id(self, id):
-    pass
+  def get_element_by_id(self, target_id):
+    return self._search(self.root,target_id)
+  
+  def _search(self, node, target_id):
+        if node.get('id') == target_id:
+            return node
+        for child in node.get('children', []):
+            result = self._search(child, target_id)
+            if result:
+                return result
+        return None
